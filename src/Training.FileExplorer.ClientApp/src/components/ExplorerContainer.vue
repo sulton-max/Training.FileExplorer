7e7f6773761f2babb25ea025f94e4e1c05aa2389
Max <sultonbek.rakhimov@gmail.com>
2023-10-14 16:35:19 +0500

    Add explorer view with background

---
+++ src/Training.FileExplorer.ClientApp/src/components/ExplorerContainer.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div>
+
+    <!-- Action section -->
+
+
+    <!-- Explorer Items section -->
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ src/Training.FileExplorer.ClientApp/src/views/ExplorerView.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div class="relative w-full h-full">
+
+    <!-- Main background -->
+    <img class="absolute top-0 left-0 z-20 object-cover" src="@/assets/images/background.jpg" alt="Background image">
+
+    <!-- Explorer view and grid -->
+    <explorer-container/>
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+import ExplorerContainer from "@/components/ExplorerContainer.vue";
+
+</script>
